b5ea962ac80c1aa72a9b6886d12f893080a0bbd4
Thomas Vanhoutte <thomas.vanhoutte@mateco.eu>
2021-02-02 20:05:00 +0100

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'apak';
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faMapMarkerAlt = faMapMarkerAlt;
 }
